c54630b641df535c48508029c42371d50e2bf4bd
Jaguarjade <manager@hiddenhollowjackson.com>
2025-02-01 18:19:52 -0700

    Fixed template path

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
+import os
 
-# Ensure Flask looks in the "templates" folder
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates")  # Explicitly set template folder
 
 @app.route('/')
 def home():
